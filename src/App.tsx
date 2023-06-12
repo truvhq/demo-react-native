ac36234b31242904b49c83f7e0eea3e540c04d9d
Sergey N <sergnek94@gmail.com>
2023-06-12 13:01:36 -0400

    fix status bar for dark theme (#32)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
 import { RootSiblingParent } from 'react-native-root-siblings';
 import { RecoilRoot } from 'recoil';
 
@@ -10,6 +11,7 @@ export const App = () => {
   return (
     <RootSiblingParent>
       <RecoilRoot>
+        <StatusBar barStyle="dark-content" />
         <NavigationContainer>
           <Navigation />
         </NavigationContainer>
